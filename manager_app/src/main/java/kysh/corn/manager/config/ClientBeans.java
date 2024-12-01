d0b71389c37fc7defef0d2ada62e7e6d3cbcb0c6
kushlenkov <kushlenkov.av@gmail.com>
2024-12-02 00:23:48 +0300

    feat: SC24EP04 (oauth) complete

--- manager_app/src/main/java/kysh/corn/manager/config/ClientBeans.java
+++ manager_app/src/main/java/kysh/corn/manager/config/ClientBeans.java
@@ -1,9 +1,13 @@
 package kysh.corn.manager.config;
 
 import kysh.corn.manager.client.RestClientProductsRestClient;
+import kysh.corn.manager.security.OAuthClientRequestInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -11,12 +15,17 @@ public class ClientBeans {
 
     @Bean
     public RestClientProductsRestClient productsRestClient(
-            @Value("${selmag.services.catalogue.uri:http://localhost:8081}") String catalogueBaseUri) {
+            @Value("${selmag.services.catalogue.uri:http://localhost:8081}") String catalogueBaseUri,
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository,
+            @Value("${selmag.services.catalogue.registration-id:keycloak}") String registrationId) {
 
-        return new RestClientProductsRestClient(
-                RestClient
-                        .builder()
-                        .baseUrl(catalogueBaseUri)
-                        .build());
+        return new RestClientProductsRestClient(RestClient.builder()
+                .baseUrl(catalogueBaseUri)
+                .requestInterceptor(
+                        new OAuthClientRequestInterceptor(
+                                new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository,
+                                        authorizedClientRepository), registrationId))
+                .build());
     }
 }
